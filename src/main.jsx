b804917522b2977cd12316d103360061f8ecbb8b
robandivya <robandivya@outlook.com>
2024-09-11 12:13:18 +0530

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <DndProvider backend={HTML5Backend}>
+    <App />
+  </DndProvider>
+);
